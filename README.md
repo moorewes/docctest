84f4f3071c1d20c88f5ddbb3e724ddc3dc5257d6
Wes Moore <wmoore@firstorion.com>
2023-04-27 09:19:28 -0700

    Add package

---
+++ README.md
@@ -0,0 +1,3 @@
+# docctest
+
+A description of this package.
---
+++ Sources/docctest/docctest.swift
@@ -0,0 +1,7 @@
+/// Name of parent class
+public struct docctest {
+    public private(set) var text = "Hello, World!"
+    /// Create
+    public init() {
+    }
+}
